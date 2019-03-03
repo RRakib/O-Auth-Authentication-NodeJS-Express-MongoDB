4c45b78141f773bfd9a9cc692ff8d6a8ec92fb86
Rakib Uddin <16103030@iubat.edu>
2019-03-03 22:31:47 +0600

    Cookie-session added

--- Config/passport-setup.js
+++ Config/passport-setup.js
@@ -3,6 +3,16 @@ const keys = require("./keys")
 const GoogleStrategy = require("passport-google-oauth20")
 const users = require("../Models/models")
 
+passport.serializeUser((user , done) => {
+    done(null , user.id)
+}) 
+
+passport.deserializeUser((id , done) => {
+    users.findOne(id)
+        .then(res => {
+            done(null , res.id)
+        })
+})
 
 passport.use(
     new GoogleStrategy({
@@ -11,9 +21,24 @@ passport.use(
         clientSecret : keys.google.clientSecret
     } , (accessToken , refreshToken , profile , done) => {
             console.log("Hitting the profile");
-            users.find({ gid : profile.id})
+            users.findOne({ gid : profile.id})
                 .then(res => {
-                    console.log(res)
+                    if(res){
+                        console.log("User Already Exist");
+                        done(null , res)
+                    }
+                    else{
+                        users({
+                            gid : profile.id,
+                            name: profile.displayName
+                        })
+                        .save()
+                        .then(resData => {
+                            console.log("Data saved")
+                            done(null , resData)
+                        })
+                        .catch(err => console.log(err))
+                    }
                 })
                 .catch(err => {
                     console.log("OPPS! " + err)
--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const route = require("./Controller/auth-router");
 const passportSetup = require("./Config/passport-setup")
 const mongoose = require("mongoose");
 const keys = require("./Config/keys")
+const passport = require("passport");
+const cookieSession = require("cookie-session");
 const app = express();
 
 
 // Connect To MongoDB
-mongoose.connect(keys.mongoDB.dbURL)
+mongoose.connect(keys.mongoDB.dbURL , {useNewUrlParser : true})
     .then(res => console.log("Connected to MongoDB"))
     .catch(err => console.log("Opps! Error!!! " + err))
 
@@ -19,6 +21,16 @@ app.use("/auth/" , route)
 
 // View Engine
 app.set("view engine" , "ejs");
+
+
+app.use(cookieSession({
+    maxAge : 24 * 60 * 60 * 1000,
+    keys : [keys.cookie]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session())
+
 
 // Static File
 app.use(express.static("Public/"))
